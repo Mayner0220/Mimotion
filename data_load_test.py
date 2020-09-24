9b0f18f83771326fdabc6dee4fcbec9defe60e8f
Mayner <shj030220@gmail.com>
2020-09-24 20:25:23 +0900

    [Commit] Commit All

--- data_load_test.py
+++ data_load_test.py
@@ -4,7 +4,6 @@ from glob import glob
 from PIL import Image
 import numpy as np
 
-print(glob(r'C:\U*'))
 path = "dataset/train/*/*.jpg"
-data = glob(r"dataset/train/*/*.jpg")
-print(data)
+# print(glob("dataset/train/*/*.jpg"))
+print(glob(path))
