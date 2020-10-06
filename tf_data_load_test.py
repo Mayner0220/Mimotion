452d3a724c388b949bead860a4392bb9fdd09324
Mayner <shj030220@gmail.com>
2020-10-06 15:03:07 +0900

    [Commit] Commit All

--- tf_data_load_test.py
+++ tf_data_load_test.py
@@ -9,11 +9,9 @@ data_width = 48
 channel_n = 1
 
 train_image_list = []
-train_label_list = []
 train_label_name_list = []
 
 test_image_list = []
-test_label_list = []
 test_label_name_list = []
 
 # Train dataset
@@ -33,9 +31,16 @@ def read_image(path):
     image = np.array(Image.open(path))
     return image.reshape(image.shape[0], image.shape[1], 1)
 
+# Return image with its label
+def _read_py_function(path, label):
+    image = read_image(path)
+    label = np.array(label, dtype=np.uint8)
+
+    return image.astype(np.int32), label
+
 def dataset_map(dataset, image_list, label_list):
     dataset = dataset.map(lambda image_list, label_list: tuple(
-        tf.py_function([image_list, label_list], [tf.int32, tf.uint8])
+        tf.py_function(_read_py_function, [image_list, label_list], [tf.int32, tf.uint8])
     ))
 
     return dataset
@@ -46,8 +51,11 @@ for path in train_data:
 for path in test_data:
     test_label_name_list.append(get_label(path))
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_image_list, train_label_list))
-test_dataset = tf.data.Dataset.from_tensor_slices((test_image_list, test_label_list))
+train_dataset = tf.data.Dataset.from_tensor_slices((train_image_list, train_label_name_list))
+test_dataset = tf.data.Dataset.from_tensor_slices((test_image_list, test_label_name_list))
 
-map_train_dataset = dataset_map(train_dataset, read_image(train_data), get_label(train_label_name_list))
-map_test_dataset = dataset_map(test_dataset, read_image(test_data), get_label(test_label_name_list))
+# read_image() 부분에서 에러 발생
+# map_train_dataset = dataset_map(train_dataset, read_image(train_data), get_label(train_label_name_list))
+# map_test_dataset = dataset_map(test_dataset, read_image(test_data), get_label(test_label_name_list))
+
+map_train_dataset = train_dataset.map(lambda train_data, train_label_name_list: tuple())
