bdc1588c58145a181c2409422d933371692054b2
Mayner <shj030220@gmail.com>
2020-10-18 01:18:07 +0900

    [Feat] Rewrite the entire code

--- create_tfrecords_from_data.py
+++ create_tfrecords_from_data.py
@@ -1,9 +1,3 @@
-import os
-import sys
-import glob
-import pickle
-import shutil
-import argparse
 import tensorflow as tf
 
 def _bytes_feature(value):
@@ -17,6 +11,23 @@ def _float_feature(value):
 def _int64_feature(value):
     """bool, enum, in, uint 타입을 받아서 int64 list를 리턴"""
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+def serialize_example(feature0, feature1, feature2, feature3):
+    """파일에 write 하기 위한 tf.Example messange 생성"""
+
+    # Key: feature name
+    # Value: tf.Example에 적합한 타입
+    feature = {
+        "feature0": _int64_feature(feature0),
+        "feature1": _int64_feature(feature1),
+        "feature2": _bytes_feature(feature2),
+        "feature3": _float_feature(feature3)
+    }
+
+    # tf.train.Example을 통해 Feature message를 생성
+    example_proto = tf.train.Example(features=tf.train.Features(feature=feature))
+
+    return example_proto.SerializeToString()
 
 
 """DUMP CODE"""
