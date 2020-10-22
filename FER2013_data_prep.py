05923138dc151f9fc355fb9a14e05ceafd5aff58
Shim Jung Hyun <shj030220@gmail.com>
2020-10-22 15:57:34 +0900

    [Feat] Add showing information about dataset

--- FER2013_data_prep.py
+++ FER2013_data_prep.py
@@ -25,7 +25,6 @@ Test_neutral = len(os.walk(test_dir + "/neutral").__next__()[2])
 Test_disgust = len(os.walk(test_dir + "/disgust").__next__()[2])
 Test_surprise = len(os.walk(test_dir + "/surprise").__next__()[2])
 
-
 generator = ImageDataGenerator(rescale=1./255)
 
 train_generator = generator.flow_from_directory(
@@ -36,10 +35,28 @@ train_generator = generator.flow_from_directory(
     class_mode="categorical"
 )
 
-test_generator = generator.flow_from_directory(
+validation_generator = generator.flow_from_directory(
     test_dir,
     target_size=(48,48),
     batch_size=batch_size,
     color_mode="grayscale",
     class_mode="categorical"
 )
+
+print("\n=======================[Train Dataset Information]=======================")
+print("train: sad -", Train_sad)
+print("train: fear -", Train_fear)
+print("train: angry -", Train_angry)
+print("train: happy -", Train_happy)
+print("train: neutral -", Train_neutral)
+print("train: disgust -", Train_disgust)
+print("train: surprise -", Train_surprise)
+
+print("\n=======================[Test Dataset Information]=======================")
+print("test: sad -", Test_sad)
+print("test: fear -", Test_fear)
+print("test: angry -", Test_angry)
+print("test: happy -", Test_happy)
+print("test: neutral -", Test_neutral)
+print("test: disgust -", Test_disgust)
+print("test: surprise -", Test_surprise)
