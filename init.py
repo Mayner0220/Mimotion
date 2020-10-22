9eeae64a8ad4a9c3f2c60fc4d6e301fa1e9c785d
Shim Jung Hyun <shj030220@gmail.com>
2020-10-22 16:06:03 +0900

    init

--- init.py
+++ init.py
@@ -1,3 +1,21 @@
 """
+이 프로젝트의 코드들은 다음 주소의 내용을 참조하여 작성되었습니다.
+The code for this project was written with reference to the following address.
 Reference: https://github.com/atulapra/Emotion-detection/blob/master/src/emotions.py
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+coding=utf-8
+License: Apache-2.0
+Dataset: FER-2013
 """
