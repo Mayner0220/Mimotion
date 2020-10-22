687729ddcf65208954fb45de2c8a352ca5024b4a
Shim Jung Hyun <shj030220@gmail.com>
2020-10-22 14:31:34 +0900

    [Commit] Commit

---
+++ plot_history.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def plot_history(model_history):
+    fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+
+    # accuracy history 요약
+    axs[0].plot(range(1, len(model_history.history["accuracy"])+1, model_history.history["accuracy"]))
+    axs[0].plot(range(1, len(model_history.history["test_accuracy"])+1, model_history.history["val_accuracy"]))
+
+    axs[0].set_title("Model Accuracy")
+    axs[0].set_ylabel("Accuracy")
+    axs[0].set_xlabel("Epoch")
+
+    axs[0].set_xticks(np.arange(1,len(model_history.history['accuracy'])+1),len(model_history.history['accuracy'])/10)
+
+    axs[0].legend(["train", "test"], loc="best")
+
+    # loss history 요약
+    axs[1].plot(range(1, len(model_history.history["loss"]+1), model_history.history["loss"]))
+    axs[1].plot(range(1, len(model_history.history["test_loss"]+1), model_history.history["test_loss"]))
+
+    axs[1].set_title("Model Loss")
+    axs[1].set_ylabel("Loss")
+    axs[1].set_xlabel("Epoch")
+
+    axs[1].set_xticks(np.arange(1,len(model_history.history['loss'])+1),len(model_history.history['loss'])/10)
+
+    axs[1].legend(["train", "test"], loc="best")
+
+    fig.savefig("plot.png")
+    plt.show()
